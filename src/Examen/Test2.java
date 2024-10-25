c58687b60a9eca4f6cfaa29934062f520fb6c047
Brandy <Brandy@VOOK.bbrouter>
2024-10-25 12:43:12 -0600

    JavaFx- Table View Sample es un ejemplo de un directorio

---
+++ src/Examen/Test2.java
@@ -0,0 +1,12 @@
+
+package Examen;
+
+public class Test2 {
+    public static void main(String...args) {
+        for (int i = 0; i < 20; i++) {
+            if ( i % 2 == 1 && i > 3 && i < 15){
+                System.out.println(i);
+            }
+        }
+    }
+}
